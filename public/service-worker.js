74b50cb5271c96b7cf66c2236f6b59b0d5f7f950
Chau Pham <ncp9988@gmail.com>
2022-03-06 12:17:53 -0600

    fix service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
 const APP_PREFIX = 'PWA-BudgetTracker';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX;
+const DATA_CACHE_NAME = "data-cache-" + VERSION;
 
 const FILE_TO_CACHE = [
    "/",
@@ -20,24 +21,6 @@ const FILE_TO_CACHE = [
     
 ]
 
-// Respond with cached resources
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
-
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
-    })
-  )
-})
 
 // Cache resources
 self.addEventListener('install', function (e) {
@@ -70,4 +53,60 @@ self.addEventListener('activate', function (e) {
     })
   );
 });
+
+// Respond with cached resources
+self.addEventListener("fetch", function(event) {
+  // cache all get requests to /api routes
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(event.request);
+          });
+      }).catch(err => console.log(err))
+    );
+    return;
+  }
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
+
+// Respond with cached resources
+// self.addEventListener('fetch', function (e) {
+//   console.log('fetch request : ' + e.request.url)
+//   e.respondWith(
+//     caches.match(e.request).then(function (request) {
+//       if (request) { // if cache is available, respond with cache
+//         console.log('responding with cache : ' + e.request.url)
+//         return request
+//       } else {       // if there are no cache, try fetching request
+//         console.log('file is not cached, fetching : ' + e.request.url)
+//         return fetch(e.request)
+//       }
+
+//       // You can omit if/else for console.log & put one line below like this too.
+//       // return request || fetch(e.request)
+//     })
+//   )
+// })
+
 
